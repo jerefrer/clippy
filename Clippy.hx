88f55cf12b26ab467c1db3874fce5ea7166df0f1
Jérémy FRERE <frere.jeremy@gmail.com>
2011-12-09 14:21:35 +0100

    Added Twipsy support, resized to 16x16, removed label and added skype icon (just change the library.xml if needed)

--- Clippy.hx
+++ Clippy.hx
@@ -1,9 +1,6 @@
 import flash.display.MovieClip;
 import flash.events.MouseEvent;
 import flash.display.SimpleButton;
-import flash.text.TextField;
-import flash.text.TextFieldAutoSize;
-import flash.text.TextFormat;
 import flash.external.ExternalInterface;
 
 class ButtonUp extends MovieClip {}
@@ -13,62 +10,40 @@ class ButtonDown extends MovieClip {}
 class Clippy {
 
   static var text:String;
-  static var func:String;
-  static var defaultLabelText:String;
-  static var feedbackLabelText:String;
-  static var label:TextField;
+  static var overCallback:String;
+  static var outCallback:String;
+  static var clickCallback:String;
   static var button:SimpleButton;
-  static var format:TextFormat;
 
-  static function upFunction (e:MouseEvent) {
+  static function triggerCallback(theCallback) {
     if(ExternalInterface.available) {
       ExternalInterface.marshallExceptions = true;
-      if(func != '' && func != null) {
-        text = ExternalInterface.call(func);
+      if(theCallback != '' && theCallback != null) {
+        ExternalInterface.call(theCallback);
       }
     }
-    flash.system.System.setClipboard(text);
-    label.text = feedbackLabelText;
-    label.setTextFormat(format);
   }
 
   static function overFunction (e:MouseEvent) {
-    label.textColor = 0x666666;
+    triggerCallback(overCallback);
   }
-  
-  static function outFunction(e:MouseEvent) {
-    label.textColor = 0x888888;
-    label.text = defaultLabelText;
-    label.setTextFormat(format);
+
+  static function outFunction (e:MouseEvent) {
+    triggerCallback(outCallback);
+  }
+
+  static function downFunction (e:MouseEvent) {
+    flash.system.System.setClipboard(text);
+    triggerCallback(clickCallback);
   }
 
   // Main
   static function main() {
     text = flash.Lib.current.loaderInfo.parameters.text;
-    func = flash.Lib.current.loaderInfo.parameters.func;
-    defaultLabelText = flash.Lib.current.loaderInfo.parameters.label;
-    feedbackLabelText = flash.Lib.current.loaderInfo.parameters.feedback;
-    
-    if(defaultLabelText == null) {
-      defaultLabelText = "copy to clipboard";
-    }
-    if(feedbackLabelText == null) {
-      feedbackLabelText = "copied!";
-    }
-    
-    // label
-    
-    label = new TextField();
-    format = new TextFormat("Arial", 11);
-    
-    label.text = defaultLabelText;
-    label.setTextFormat(format);
-    label.textColor = 0x888888;
-    label.selectable = false;
-    label.x = 15;
-    
-    flash.Lib.current.addChild(label);
-    
+    overCallback = flash.Lib.current.loaderInfo.parameters.overCallback;
+    outCallback = flash.Lib.current.loaderInfo.parameters.outCallback;
+    clickCallback = flash.Lib.current.loaderInfo.parameters.clickCallback;
+        
     // button
     button = new SimpleButton();
     button.useHandCursor = true;
@@ -78,14 +53,9 @@ class Clippy {
     button.downState = new ButtonDown();
     button.hitTestState = new ButtonDown();
     
-    label.addEventListener(MouseEvent.MOUSE_UP, upFunction );
-    button.addEventListener(MouseEvent.MOUSE_UP, upFunction );
-    
-    label.addEventListener(MouseEvent.MOUSE_OVER, overFunction);
-    button.addEventListener(MouseEvent.MOUSE_OVER, overFunction);
-
-    label.addEventListener(MouseEvent.MOUSE_OUT, outFunction);
-    button.addEventListener(MouseEvent.MOUSE_OUT, outFunction);
+    button.addEventListener(MouseEvent.MOUSE_OVER, overFunction );
+    button.addEventListener(MouseEvent.MOUSE_OUT,  outFunction  );
+    button.addEventListener(MouseEvent.MOUSE_DOWN, downFunction );
     
     flash.Lib.current.addChild(button);
   }
